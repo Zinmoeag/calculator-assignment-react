602769cbcd86ebf3bdb806394689278a91872ff1
Ast_Zinmoag <astro9857@gmail.com>
2023-04-21 18:37:49 +0630

    version2

--- src/App.js
+++ src/App.js
@@ -4,32 +4,44 @@ import {
 } from "react"; 
 import "./App.css";
 
+function Btn (props) {
+  let style = {
+    margin  : "0px 4px",
+    padding : "10px 20px",
+    border: "none",
+    background : "#8dbdff",
+  };
+  return (
+    <button onClick={props.handler} value={props.value} style={style}>{props.display}</button>
+  )
+}
+
 function App() { 
   const inputRef = useRef(null); 
   const resultRef = useRef(null); 
-  const [result, setResult] = useState(0); 
- 
-  function plus(e) { 
-    e.preventDefault(); 
-    setResult((result) => result + Number(inputRef.current.value));
+  const [result, setResult] = useState(0);
 
-  }; 
+  let signs = [
+    {display : "Add" , sign : "+"},
+    {display : "Substract" , sign : "-"},
+    {display : "Multiple" , sign : "*"},
+    {display : "Divide" , sign : "/"},
+  ] 
 
- 
-  function minus(e) { 
-    e.preventDefault(); 
-    setResult((result) => result - Number(inputRef.current.value));
-  };
- 
-  function times(e) { 
-     e.preventDefault(); 
-     setResult((result) => result * Number(inputRef.current.value));
-  }; 
- 
-  function divide(e) { 
-    e.preventDefault(); 
-     setResult((result) => result / Number(inputRef.current.value)); 
-  };
+  function handleCalculation(e){
+    e.preventDefault();
+    
+    if(e.target.value === "+"){
+      setResult((result) => result + Number(inputRef.current.value));
+    }else if(e.target.value === "-"){
+      setResult((result) => result - Number(inputRef.current.value));
+    }else if(e.target.value === "*"){
+      setResult((result) => result * Number(inputRef.current.value));
+    }else if(e.target.value === "/"){
+      setResult((result) => result / Number(inputRef.current.value));
+    }
+
+  }
  
   function resetInput(e) { 
     e.preventDefault();
@@ -48,21 +60,40 @@ function App() {
         <h1>Simplest Working Calculator</h1> 
       </div> 
       <form> 
-       <p ref={resultRef}> 
+       <p 
+        ref={resultRef}
+        style={{
+            fontSize:"30px",
+        }}
+       > 
           {result} 
         </p> 
         <input
+          style ={{
+              width:"350px",
+              height:"30px",
+          }}
           pattern="[0-9]" 
           ref={inputRef} 
           type="number" 
           placeholder="Type a number" 
         /> 
-        <button onClick={plus}>add</button> 
-        <button onClick={minus}>substract</button>  
-        <button onClick={times}>multiple</button>
-        <button onClick={divide}>divide</button>
-        <button onClick={resetInput}>Reset Input</button>
-        <button onClick={resetResult}>Reset Result</button>
+
+        <br />
+
+        <div style={{marginTop:"20px"}}>
+            {signs.map( (s,i) => {
+              return(
+                <Btn handler={handleCalculation} value={s.sign} display={s.display} key={i} />
+              )
+            })}
+          
+            <Btn handler={resetInput} display="Reset Input" />
+            <Btn handler={resetResult} display="Reset Result" />
+        </div>
+
+        
+  
       </form> 
     </div> 
   ); 
